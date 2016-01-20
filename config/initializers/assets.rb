6f3c6b81d071912208b755561b9f49be4945d1b1
Julien Athomas <julienathomas@Juliens-MacBook-Pro.local>
2016-01-20 07:10:50 +0100

    changing assets config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
-Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( style.css.scss )
 Rails.application.config.assets.precompile += %w( script.js )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( jquery.min.js )
