0ee73dc0caf19ef41d599bd9aec23eea780930fb
Julien Athomas <julienathomas@Juliens-MacBook-Pro.local>
2016-01-22 15:28:34 +0100

    solving mistakes in english

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -3,8 +3,8 @@ class InterestsController < ApplicationController
   attr_accessor :myInterests
   def initialize
     @citation = "hello world"
-    @myInterests = [ {:title => "cooking", :image => "images/cooking.jpg", :description => "I like cooking and baking everytime I can to change my mind."},
-                    {:title => "sport", :image => "images/sport.jpg", :description => "I mostly do gym three times a week and Socker/Handball twice a month."}
+    @myInterests = [ {:title => "cooking", :image => "images/cooking.jpg", :description => "I like cooking and baking everytime I can. It enables me to change my mind."},
+                    {:title => "sport", :image => "images/sport.jpg", :description => "I go to the gym three times a week and twice a month, I play socker/handball."}
               ]
   end
   def interests
